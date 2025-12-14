3e48b7b28257fbdc088b8e5b86ee70bb586a7498
namphong144 <finntasy14@gmail.com>
2025-12-14 20:36:27 +0700

    update UI header landing and admin

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -6,10 +6,10 @@ import { Button } from "../ui/button";
 function Header() {
   return (
     <nav className="fixed top-0 right-0 left-0 z-50 px-6 py-2 border-b border-border/50 bg-background/80 backdrop-blur-md h-16">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
+      <div className="max-w-6xl mx-auto flex justify-between items-center h-full">
         <Link href="/" className="flex items-center gap-2">
-          <Image src={"/logo.png"} alt="DentWise Logo" width={32} height={32} className="w-11" />
-          <span className="font-semibold text-lg">Dentalize</span>
+          <Image src={"/logo.png"} alt="Dentalize Logo" width={102} height={102} className="w-31" />
+          {/* <span className="font-semibold text-lg">Dentalize</span> */}
         </Link>
 
         <div className="hidden md:flex items-center gap-8">
